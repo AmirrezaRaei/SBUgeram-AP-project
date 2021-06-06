7fb457fcfac0f1b7c9ed10419e497c495832e8fc
Amirreza <amir1381raei@gmail.com>
2021-06-06 21:16:03 +0430

    update the width & height number

--- src/Model/PageLoader.java
+++ src/Model/PageLoader.java
@@ -14,8 +14,8 @@ public class PageLoader {
     private static Stage stage;
     private static Scene scene;
 
-    public static final int WIDTH = 420;
-    public static final int HEIGHT = 620;
+    public static final int WIDTH = 440;
+    public static final int HEIGHT = 640;
 
     public static void initStage(Stage primaryStage) {
 
@@ -41,14 +41,14 @@ public class PageLoader {
         return fxmlLoader.load();
     }
 
-    //load fxmls with already set controllers
+    //load FXMLs with already set controllers
     public void load(String url) throws IOException {
         scene = new Scene(new PageLoader().loadFXML(url));
         stage.setScene(scene);
         stage.show();
     }
 
-    //load fxmls with a special controller set in the runtime
+    //load FXMLs with a special controller set in the runtime
     public void load(String fxml, Object controller) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/View/" + fxml + ".fxml"));
         fxmlLoader.setController(controller);
